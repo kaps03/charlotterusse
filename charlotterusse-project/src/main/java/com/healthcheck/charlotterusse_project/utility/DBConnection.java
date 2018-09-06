45fc03e4a1bf775a33a84b688d9c18b86f789e73
kapil soni; <soni.kapil2067@gmail.com>
2018-09-06 11:54:54 +0530

    making connection with properties file...

--- charlotterusse-project/src/main/java/com/healthcheck/charlotterusse_project/utility/DBConnection.java
+++ charlotterusse-project/src/main/java/com/healthcheck/charlotterusse_project/utility/DBConnection.java
@@ -1,29 +1,38 @@
 package com.healthcheck.charlotterusse_project.utility;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
-	
-	private static ConnectionUtility connectionUtility;
+
 	private static Connection connection;
+
 	private DBConnection() {
 	}
-	
+
 	public static Connection getConnection() {
 		try {
-			if(connection == null) {
-				Class.forName(ConnectionUtility.DRIVER_NAME);
-				connection = DriverManager.getConnection(ConnectionUtility.URL , ConnectionUtility.USERNAME, ConnectionUtility.PASSWORD);				
+			if (connection == null) {
+				FileReader reader = new FileReader("db.properties");
+				Properties properties = new Properties();
+				properties.load(reader);
+				Class.forName(properties.getProperty("sqlserver_driver_name"));
+				connection = DriverManager.getConnection(properties.getProperty("sqlserver_url"),
+						properties.getProperty("sqlserver_username"), properties.getProperty("sqlserver_password"));
 			}
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
+		} catch (IOException exception) {
+
 		}
-		
+
 		return connection;
-		
-	} 
+
+	}
 }
